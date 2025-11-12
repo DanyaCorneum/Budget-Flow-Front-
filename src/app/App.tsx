b3df5d026b6ca62bb50e05f612321b83e4db1cd8
Ratkov_Daniil <daniilratkov3@gmail.com>
2025-11-12 16:49:58 +0700

    The architecture was created according to the FSD, and the Button and Input components were also created.  commit 1

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import {Button, Input} from "../shared";
+
+function App() {
+
+  return (
+    <>
+      <Button>click me</Button>
+        <Input isValid={true}/>
+    </>
+  )
+}
+
+export default App
