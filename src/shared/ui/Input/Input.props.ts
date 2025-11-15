59086e3e39ed6d1db57500ad713e431c955114a0
Ratkov_Daniil <daniilratkov3@gmail.com>
2025-11-15 15:41:35 +0700

    changes on input

--- src/shared/ui/Input/Input.props.ts
+++ src/shared/ui/Input/Input.props.ts
@@ -3,4 +3,5 @@ import  {type HTMLAttributes} from "react";
 export interface InputProps extends HTMLAttributes<HTMLInputElement> {
     isValid: boolean;
     className?: string;
+    placeholder?: string;
 }
--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -4,9 +4,9 @@ import cn from "classnames";
 import styles from "./Input.module.scss"
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-    function Input({className="", isValid=true, ...props}, ref) {
+    function Input({className="", isValid=true, placeholder="placeholder", ...props}, ref) {
         return (
-            <input ref={ref} {...props} className={cn(styles["input"], className)}/>
+            <input placeholder={placeholder} ref={ref} {...props} className={cn(styles["input"], className)}/>
         )
     }
 )
